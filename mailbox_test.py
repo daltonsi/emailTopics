f2c15ca75779fe8bb25dabf961fb3cf96b917b52
Dalton Simancek <simancek@umich.edu>
2016-12-02 13:22:59 -0500

    update mailbox with dictionary/json encoding
    update mailbox with dictionary/json encoding

--- mailbox_test.py
+++ mailbox_test.py
@@ -1,4 +1,6 @@
-import mailbox  ##I made an update
+import mailbox
+import json
+import time
 
 
 # CODE Reference: http://stackoverflow.com/questions/7166922/extracting-the-body-of-an-email-from-mbox-file-decoding-it-to-plain-text-regard
@@ -43,14 +45,26 @@ def getbodyfromemail(msg):
 
 
 if __name__ == '__main__':
+    time.clock()
+    list_of_email_dicts = []
     for thisemail in mailbox.mbox('UMSI-Open.mbox'):
+
+        new_dict = {}
         body = getbodyfromemail(thisemail)
-        print '******************************************'
         print thisemail['subject']
-        print thisemail['Date']
-        if thisemail['To']:
-            print "TO: " + thisemail['To']
-        if thisemail['From']:
-            print "FROM: " + thisemail['From']
-        print
+        print thisemail['To']
+        print thisemail['From']
         print body
+        #if themail['body'] == None:
+        new_dict["body"] = body.encode('utf-8')
+        #else:
+    #        new_dict["body"] = body.encode('utf-8')
+
+        new_dict["subject"] = thisemail['subject'].encode('utf-8')
+        new_dict["To"] = thisemail['To'].encode('utf-8')
+        new_dict['From'] = thisemail['From'].encode('utf-8')
+        list_of_email_dicts.append(new_dict)
+
+    with open('emails_result.json', 'w') as fp:
+        json.dump(list_of_email_dicts, fp)
+    time.clock()
